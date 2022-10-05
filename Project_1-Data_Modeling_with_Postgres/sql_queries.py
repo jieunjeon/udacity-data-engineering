18f0fd25e0aef848ad417fa52d23eaf2cb2403b9
Jieun Jeon <jieunjeon1023@gmail.com>
2022-10-05 23:53:10 +0900

    add etl functions and update schema

--- Project_1-Data_Modeling_with_Postgres/sql_queries.py
+++ Project_1-Data_Modeling_with_Postgres/sql_queries.py
@@ -10,12 +10,12 @@ time_table_drop = "DROP TABLE IF EXISTS time" # dimension table
 
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
-    songplay_id serial PRIMARY KEY, 
-    start_time bigint NOT NULL, 
+    songplay_id SERIAL PRIMARY KEY, 
+    start_time timestamp NOT NULL, 
     user_id int NOT NULL,
     level varchar NOT NULL, 
-    song_id varchar, 
     artist_id varchar, 
+    song_id varchar, 
     session_id int, 
     location varchar,
     user_agent varchar);
@@ -56,31 +56,55 @@ CREATE TABLE IF NOT EXISTS time (
     week int NOT NULL, 
     month int NOT NULL, 
     year int NOT NULL, 
-    weekday int NOT NULL);
+    weekday varchar NOT NULL);
     """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-""")
+INSERT INTO songplays (
+    start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (songplay_id) DO NOTHING 
+    ;""")
 
 user_table_insert = ("""
-""")
+INSERT INTO users (
+    user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level
+    ;""")
 
 song_table_insert = ("""
-""")
+INSERT INTO songs (
+    song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) DO NOTHING 
+    ;""")
 
 artist_table_insert = ("""
-""")
-
+INSERT INTO artists (
+    artist_id, name, location, latitude, longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING 
+    ;""")
 
 time_table_insert = ("""
-""")
+INSERT INTO time (
+    start_time, hour, day, week, month, year, weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO NOTHING 
+    ;""")
 
 # FIND SONGS
 
 song_select = ("""
-""")
+SELECT s.song_id, a.artist_id 
+    FROM songs s
+    JOIN artists a
+    ON s.artist_id = a.artist_id
+    WHERE s.title = (%s) AND a.name = (%s) AND s.duration = (%s)
+    ;""")
 
 # QUERY LISTS
 
